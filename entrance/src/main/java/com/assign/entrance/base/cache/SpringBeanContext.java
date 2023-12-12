df903b78d5df9c5076900e1120c6ff25e6ce9c4f
li2464r <li2464r@163.com>
2023-12-12 11:43:23 +0800

    优化打包

--- entrance/src/main/java/com/assign/entrance/base/cache/SpringBeanContext.java
+++ entrance/src/main/java/com/assign/entrance/base/cache/SpringBeanContext.java
@@ -3,21 +3,21 @@ package com.assign.entrance.base.cache;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBeanContext <T> implements ApplicationContextAware {
+public class SpringBeanContext<T> implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
 
     @Override
-    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         SpringBeanContext.applicationContext = applicationContext;
     }
 
     public static <T> T getBean(String beanName) {
+        System.out.println("getBean----" + beanName);
         return (T) applicationContext.getBean(beanName);
     }
 
--- entrance/src/main/java/com/assign/entrance/service/impl/UrbanRuralServiceImpl.java
+++ entrance/src/main/java/com/assign/entrance/service/impl/UrbanRuralServiceImpl.java
@@ -160,9 +160,9 @@ public class UrbanRuralServiceImpl extends ServiceImpl<UrbanRuralMapper, UrbanRu
     }
 
     /**
-     * 北京市 天津市 河北省 山西省 内蒙古自治区 辽宁省 吉林省 黑龙江省 上海市 江苏省 浙江省 安徽省 福建省 江西省 山东省 河南省 湖北省 湖南省 广东省 广西壮族自治区 海南省 重庆市 四川省 贵州省
-     * TODO 云南省
-     * 西藏自治区 陕西省 甘肃省 青海省 宁夏回族自治区 新疆维吾尔自治区
+     * 北京市 天津市 河北省 山西省 内蒙古自治区 辽宁省 吉林省 黑龙江省 上海市 江苏省 浙江省 安徽省 福建省 江西省 山东省 河南省 湖北省 湖南省 广东省 广西壮族自治区 海南省 重庆市 四川省 贵州省 云南省
+     * TODO 西藏自治区
+     * 陕西省 甘肃省 青海省 宁夏回族自治区 新疆维吾尔自治区
      *
      * @return 城市列表
      */
@@ -262,7 +262,7 @@ public class UrbanRuralServiceImpl extends ServiceImpl<UrbanRuralMapper, UrbanRu
                 villageCode = villageElement.select("td").get(1).text();
             }
             // 保存
-            // saveOrUpdate(parentId, cityCode, cityName, areaCodeParent, level, villageCode);
+            saveOrUpdate(parentId, cityCode, cityName, areaCodeParent, level, villageCode);
         });
     }
 
@@ -272,7 +272,7 @@ public class UrbanRuralServiceImpl extends ServiceImpl<UrbanRuralMapper, UrbanRu
         Document document = null;
         try {
             init();
-            Thread.sleep(500);
+            Thread.sleep(1000);
             document = Jsoup.connect(url).timeout(5 * 60 * 1000).get();
         } catch (Exception e) {
             logger.error("Error connecting {}", url, e);
